44c2fd86e40ee3bccafe6b8541579cf299eff6f4
sergioBustamante <sergio.bustamante@niceincontact.com>
2024-08-26 13:30:34 -0400

    App revisa bisiestos 100 Y 400

--- src/bisiesto.js
+++ src/bisiesto.js
@@ -1,5 +1,5 @@
 function generarBisiesto(year){
-    if(year % 400 == 0)
+    if(year % 400 == 0&&year % 100 == 0)
         {
           year="Bisiesto"
         }
--- src/bisiesto.spec.js
+++ src/bisiesto.spec.js
@@ -7,4 +7,13 @@ describe("AñoBisiesto", () => {
 it("generar bisiesto", () => {
     expect(generarBisiesto(400)).toEqual("Bisiesto");
 });
+it("generar no bisiesto", () => {
+    expect(generarBisiesto(1700)).toEqual("No bisiesto");
+});
+it("generar no bisiesto", () => {
+    expect(generarBisiesto(1800)).toEqual("No bisiesto");
+});
+it("generar no bisiesto", () => {
+    expect(generarBisiesto(1900)).toEqual("No bisiesto");
+});
 });
